7bf6f76db5470d73b99974ffa7c43aefccfae786
Julian Kohlman <juliankohlman@gmail.com>
2017-12-14 11:45:52 -0500

    BST passing tests for insert/contains

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,30 +1,76 @@
-// https://msdn.microsoft.com/en-us/library/aa289150(v=vs.71).aspx
-/* eslint-disable global-require */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-trailing-spaces */
+/* eslint-disable class-methods-use-this */
+
 class BinarySearchTree {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
+  constructor(value) { // Root only special in terms of references (each node is a tree itself)
+    this.value = value; // actual node value
+    this.left = null; // link to left node
+    this.right = null; // link to right node
   }
   // Wraps the input value in a new BinarySearchTree and
   // assigns it to either the left or right subtree,
   // depending on its value
-  insert(value) {
+  insert(value) { // delegate behaviors to the child trees
+    // const nextTree = new BinarySearchTree(value);
+    // console.log(this); // this === parent
+    // console.log(nextTree); // nexxtTree === child
+    // console.log(value); // the value we want to insert into the tree (make a new node with)
 
+    if (value < this.value) {
+      // is there a left
+      if (this.left) { // the left node exists
+        this.left.insert(value); // insert the value as the child of the this.left
+      } else {
+        this.left = new BinarySearchTree(value);
+      }
+      // this.left.insert(value);
+    } else if (value > this.value) {
+      if (this.right) {
+        this.right.insert(value);
+      } else {
+        this.right = new BinarySearchTree(value);
+      }
+    }
   }
+
+
   // Checks the binary search tree for the input target
   // Can be written recursively or iteratively
   contains(target) {
+    console.log(target);
+    console.log(this);
+    console.log(this.value);
+    const find = (t) => {
+      // compare target to this.value
+      // if target is === this.value return TRUE (base case)
+      // if target is < this.value
+        // if this.value.left
+          // recurse with this.value.left
+        // else return false;
+      //
+      if (this.value === t) return true; // base case
 
+      if (t < this.value) { // smaller than top of tree GO LEFT
+        if (this.left) {
+          return this.left.contains(t);
+        }
+        return false;
+      }   // bigger than top of tree GO RIGHT
+      if (this.right) {
+        return this.right.contains(t);
+      }
+      return false;
+    };
+    return find(target);
   }
+
   // Traverses the tree in a depth-first manner, i.e. from top to bottom
   // Applies the given callback to each tree node in the process
   depthFirstForEach(cb) {
 
   }
-  // Traverses the tree in a breadth-first manner, i.e. in layers, starting 
+  // Traverses the tree in a breadth-first manner, i.e. in layers, starting
   // at the root node, going down to the root node's children, and iterating
   // through all those nodes first before moving on to the next layer of nodes
   // Applies the given callback to each tree node in the process
@@ -36,3 +82,6 @@ class BinarySearchTree {
 }
 
 module.exports = BinarySearchTree;
+// const myTree = new BinarySearchTree(6)
+// myTree.insert(5)
+// myTree.children[0]
--- src/tree.js
+++ src/tree.js
@@ -1,20 +1,41 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+
+
 class Tree {
   constructor(value) {
     this.value = value;
     this.children = [];
   }
-  // Adds a new Tree node with the input value to the current Tree node 
+  // Adds a new Tree node with the input value to the current Tree node
   addChild(value) {
-
+    // const newNode = new Tree(value);
+    // this.children.push(newNode);
+    this.children.push(new Tree(value));
   }
   // Checks this node's children to see if any of them matches the given value
   // Continues recursively until the value has been found or all of the children
   // have been checked
-  contains(value) {
-
+  contains(value) { // BFS or DFS??
+    if (this.value === value) return true;
+    for (let i = 0; i < this.children.length; i += 1) {
+      if (this.children[i].contains(value)) return true;
+    }
+    return false;
   }
 }
 
 module.exports = Tree;
+
+// run test trees down here!!
+// const myTree = new Tree(2);
+// myTree.addChild(7);
+// myTree.addChild(5); //
+// myTree[0].addChild(2);
+/*
+    2
+  7   5
+2
+
+*/
